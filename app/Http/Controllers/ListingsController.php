2eff129a42145202411b1dde7a0d6a8449c0b8bb
div-antk <51283468+owaii@users.noreply.github.com>
2020-04-13 20:37:33 +0900

    create controller & model, add route

--- app/Http/Controllers/ListingsController.php
+++ app/Http/Controllers/ListingsController.php
@@ -2,19 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Listing;
+use Auth;
+use Validator;
+
 use Illuminate\Http\Request;
 
 class ListingsController extends Controller
 {
+    // public function __construct()
+    // {
+    //     this->middleware('auth');
+    // }
+
     public function index()
     {
-        $listings = [
-            (object) [
-                'id' => 1,
-                'title' => 'タイトル',
-                'shop' => '店',
-                'tag' => 
-            ]
-        ]
+        $listings = Listing::where('user_id', Auth::user()->id)
+            ->orderBy('created_at', 'asc')
+            ->get();
+
+            return view('listing/index', ['listings' => $listings]);
     }
 }
